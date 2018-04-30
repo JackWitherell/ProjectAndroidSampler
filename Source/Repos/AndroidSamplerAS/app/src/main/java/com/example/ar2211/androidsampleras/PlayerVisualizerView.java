8de0137309ade188b50a52fb65b5b10fdfcf0abe
Unknown <JackWitherell@gmail.com>
2018-04-29 17:25:11 -0700

    PermissionRequesting Done
    Confirmed

--- Source/Repos/AndroidSamplerAS/app/src/main/java/com/example/ar2211/androidsampleras/PlayerVisualizerView.java
+++ Source/Repos/AndroidSamplerAS/app/src/main/java/com/example/ar2211/androidsampleras/PlayerVisualizerView.java
@@ -3,7 +3,6 @@ package com.example.ar2211.androidsampleras;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.Log;
